c809ede159dee70280434cad20de3c49e5953265
Matthew Willard <MWillard85@gmail.com>
2019-08-08 15:18:45 -0600

    updating for heroku

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
-    export default {
-    API_ENDPOINT: `http://localhost:3000`
-  }
+export default {
+  API_ENDPOINT: `https://sheltered-brushlands-82423.herokuapp.com/`,
+  API_KEY: process.env.REACT_APP_API_KEY,
+}
