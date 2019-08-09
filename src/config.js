9b61ddafe435f59f28ad8181ab2244b03dfcf90b
Matthew Willard <MWillard85@gmail.com>
2019-08-09 00:25:52 -0600

    small fix

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: `https://cors-anywhere.herokuapp.com/https://sheltered-brushlands-82423.herokuapp.com/api`,
+  API_ENDPOINT: `https://sheltered-brushlands-82423.herokuapp.com/api`,
   API_KEY: process.env.REACT_APP_API_KEY,
 }
